002d82c4dca142236668b25e215b8b74f4d4f29a
Ciro S. Costa <cscosta@pivotal.io>
2019-04-11 13:17:52 -0400

    add issuing of requests when performing resolution
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -2,8 +2,10 @@ package resolver
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/cirocosta/go-mod-license-finder/parser"
+	"github.com/pkg/errors"
 )
 
 type Location struct {
@@ -11,6 +13,39 @@ type Location struct {
 	URL string
 }
 
+func doRequest(ctx context.Context, url string) (resp *http.Response, err error) {
+	var req *http.Request
+
+	req, err = http.NewRequest("GET", url, nil)
+	if err != nil {
+		err = errors.Wrapf(err, "couldn't create request obj")
+		return
+	}
+
+	req = req.WithContext(ctx)
+	queryParams := req.URL.Query()
+	queryParams.Add("go-get", "1")
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		err = errors.Wrapf(err, "failed to send http request")
+		return
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		err = errors.Errorf("non-success status code %d", resp.StatusCode)
+		return
+	}
+
+	return
+}
+
 func Resolve(ctx context.Context, dependency parser.Line) (loc Location, err error) {
+	_, err = doRequest(ctx, dependency.Dependency)
+	if err != nil {
+		err = errors.Wrapf(err, "failed to issue request for dependency - %+v", dependency)
+		return
+	}
+
 	return
 }
